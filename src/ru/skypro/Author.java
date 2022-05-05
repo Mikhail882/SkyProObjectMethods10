561da1285be95aec171d4517cf65e0247ca64c64
Mikhail882 <m.pojidaev@mail.ru>
2022-05-05 13:55:00 +0300

    homework

--- src/ru/skypro/Author.java
+++ src/ru/skypro/Author.java
@@ -1,5 +1,7 @@
 package ru.skypro;
 
+import java.util.Objects;
+
 public class Author {
     public String authorFistName;
     public String authorSecondName;
@@ -17,4 +19,25 @@ public class Author {
     public String getAuthorSecondName() {
         return authorSecondName;
     }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + authorFistName + '\'' +
+                ", subname='" + authorSecondName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object a) {
+        if (this == a) return true;
+        if (a == null || getClass() != a.getClass()) return false;
+        Author author = (Author) a;
+        return authorFistName.equals(author.authorFistName) && authorSecondName.equals(author.authorSecondName);
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(authorFistName, authorSecondName);
+    }
 }
+
--- src/ru/skypro/Book.java
+++ src/ru/skypro/Book.java
@@ -1,5 +1,7 @@
 package ru.skypro;
 
+import java.util.Objects;
+
 public class Book {
     private final String bookName;
     public Author authorName;
@@ -10,16 +12,42 @@ public class Book {
         this.authorName = authorName;
         this.publishingYear = publishingYear;
     }
+
     public String getBookName() {
         return bookName;
     }
+
     public Author getAuthorName() {
         return authorName;
     }
+
     public int getPublishingYear() {
         return publishingYear;
     }
+
     public void setPublishingYear(int publishingYear) {
         this.publishingYear = publishingYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + bookName + '\'' +
+                ", author='" + authorName +
+                ", publishingYear=" + publishingYear +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object a) {
+        if (this == a) return true;
+        if (a == null || getClass() != a.getClass()) return false;
+        Book book = (Book) a;
+        return publishingYear == book.publishingYear && bookName.equals(book.bookName) && authorName.equals(book.authorName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, authorName, publishingYear);
     }
 }
